b859be96dc83da1e47e03b01865234ed765cb9cd
Priyam Gautam <98340870+Priyam117@users.noreply.github.com>
2023-11-01 11:44:34 +0530

    Day 5 HW done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
+import Navbar from './Component/Navbar';
+import Main from './Component/Main';
+import Section2 from './Component/Section2';
+import Section3 from './Component/Section3';
+import Footer from './Component/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App text-8xl">
-      hello css
+    <div className="App">
+      <Navbar />
+      <Main />
+      <Section2 />
+      <Section3 />
+      <Footer />
     </div>
   );
 }
