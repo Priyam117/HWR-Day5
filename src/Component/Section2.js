d8fac7bd45d76500a7e5687c4fcf66de2cc8e96f
Priyam Gautam <98340870+Priyam117@users.noreply.github.com>
2023-11-01 11:50:23 +0530

    make changes

--- src/Component/Section2.js
+++ src/Component/Section2.js
@@ -15,7 +15,7 @@ function Section2() {
             </h1>
             <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
             <div class="flex justify-center">
-              <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+              <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get in touch</button>
             </div>
           </div>
         </div>
