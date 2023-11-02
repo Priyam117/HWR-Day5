27ebc156054146c93e6dabe296b77a8664d895c8
Priyam Gautam <98340870+Priyam117@users.noreply.github.com>
2023-11-02 23:22:47 +0530

    make some changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+
 import React from 'react';
 import Navbar from './Component/Navbar';
 import Main from './Component/Main';
 import Section2 from './Component/Section2';
 import Section3 from './Component/Section3';
 import Footer from './Component/Footer';
+
+import QuotecardList from './Component/QuotecardList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar />
       <Main />
       <Section2 />
       <Section3 />
-      <Footer />
+      <Footer /> */}
+      <QuotecardList />
+
     </div>
   );
 }
